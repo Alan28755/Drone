450300324b756c7118643e08a90255db08147634
Alan28755 <alan2875529865@gmail.com>
2023-04-14 23:09:55 +0800

    实现了天气控制，UI交互

--- Assets/Scripts/InputManger.cs
+++ Assets/Scripts/InputManger.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManger : MonoBehaviour
 {
+    //按下ESC键呼出的菜单
     public GameObject menu;
+    private bool _menuActive = false;//菜单是否被呼出
 
-    private bool _menuActive = false;
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject editWeather;
+    public GameObject editWayPoints;
+    public GameObject editCameras;
+    private GameObject _editMenu;
+
+
+    //界面上方选项菜单的枚举
+    enum MenuButtons
     {
-
+        None,
+        Weather,
+        WayPoints,
+        Cameras
     }
 
-    // Update is called once per frame
+    private MenuButtons _menuButtons;
+
+
+    void Start()
+    {
+        menu.SetActive(false);
+        editWeather.SetActive(false);
+        editCameras.SetActive(false);
+        editWayPoints.SetActive(false);
+
+
+        _editMenu = editWeather;
+        _editMenu.SetActive(true);
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -21,5 +48,30 @@ public class InputManger : MonoBehaviour
             _menuActive = !_menuActive;
             menu.SetActive(_menuActive);
         }
+
+
+    }
+    public void ClickWeatherButton()
+    {
+        _menuButtons = MenuButtons.Weather;
+        _editMenu.SetActive(false);
+        _editMenu = editWeather;
+        _editMenu.SetActive(true);
+    }
+
+    public void ClickWayPointsButton()
+    {
+        _menuButtons = MenuButtons.WayPoints;
+        _editMenu.SetActive(false);
+        _editMenu = editWayPoints;
+        _editMenu.SetActive(true);
+    }
+
+    public void ClickCamerasButton()
+    {
+        _menuButtons = MenuButtons.Cameras;
+        _editMenu.SetActive(false);
+        _editMenu = editCameras;
+        _editMenu.SetActive(true);
     }
 }
