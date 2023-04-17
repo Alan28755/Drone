62e3831f89c4c6bc449763936926cd1be656ddd3
Alan28755 <alan2875529865@gmail.com>
2023-04-17 23:00:32 +0800

    实现了添加巡逻点和删除巡逻点的功能

--- Assets/Scripts/WayPointsEdit.cs
+++ Assets/Scripts/WayPointsEdit.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class WayPointsEdit : MonoBehaviour
 {
@@ -16,24 +18,33 @@ public class WayPointsEdit : MonoBehaviour
     [SerializeField] private GameObject uiPrefab;
     [SerializeField] private GameObject pointPrefab;
 
-    [SerializeField] private Transform drone;
+    private Transform _drone;
+
+    [SerializeField] private Button addPoint;
+
+    [SerializeField] private Button deletePoint;
+
     // Start is called before the first frame update
     void Start()
     {
         wayPoints.transform.position=Vector3.zero;
         for (int i = 0; i < wayPoints.childCount; i++)
         {
-            GameObject gameObject = Instantiate(uiPrefab);
-            gameObject.transform.SetParent(gridLayoutGroup.transform);
-            gameObject.transform.GetChild(0).GetChild(1).GetComponent<Text>().text="节点"+i.ToShortString();
+            GameObject go = Instantiate(uiPrefab,gridLayoutGroup.transform);
+            go.transform.GetChild(0).GetChild(1).GetComponent<Text>().text="节点"+i.ToShortString();
             for (int j = 0; j < 3; j++)
             {
-                gameObject.transform.GetChild(0).GetChild(0).GetChild(j).GetComponent<TMP_InputField>().
+                go.transform.GetChild(0).GetChild(0).GetChild(j).GetComponent<TMP_InputField>().
                     onValueChanged.AddListener(OnInputFieldValueChanged);
-
-
             }
         }
+
+        gridLayoutGroup.transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<Text>().text = "出发点";
+
+        addPoint.onClick.AddListener(AddPoint);
+        deletePoint.onClick.AddListener(DeletePoint);
+
+        _drone = GameObject.FindWithTag("Drone").transform;
 
 
     }
@@ -55,13 +66,54 @@ public class WayPointsEdit : MonoBehaviour
 
     }
 
-    public void AddPoint()
+    private void AddPoint()
     {
+        int count = wayPoints.childCount;
+        Transform lastPoint = wayPoints.transform.GetChild(count-1);
+        Vector3 loc = lastPoint.position;
+        float r = lastPoint.GetComponent<SphereCollider>().radius;
+        Vector3 newloc = loc + Vector3.up * r;
+        GameObject point =Instantiate(pointPrefab, newloc,quaternion.identity,wayPoints.transform);
+        count++;
+
+
+        wayPoints.GetChild(count - 2).GetComponent<DroneWaypointScript>().waypointList[0] = point;
+        point.GetComponent<DroneWaypointScript>().waypointList[0] = wayPoints.GetChild(1).gameObject;
+
+        GameObject uiPoint = Instantiate(uiPrefab, gridLayoutGroup.transform);
+        uiPoint.transform.GetChild(0).GetChild(1).GetComponent<Text>().text = "节点" + count.ToShortString();
+        //订阅
+        for (int j = 0; j < 3; j++)
+        {
+            uiPoint.transform.GetChild(0).GetChild(0).GetChild(j).GetComponent<TMP_InputField>().
+                onValueChanged.AddListener(OnInputFieldValueChanged);
+        }
 
     }
 
-    public void DeletePoint()
+    private void DeletePoint()
     {
+        int count = wayPoints.childCount;
+        //获取最后一行坐标ui
+        GameObject uiPoint = gridLayoutGroup.transform.GetChild(count - 1).gameObject;
+        //取消订阅
+        for (int j = 0; j < 3; j++)
+        {
+            uiPoint.transform.GetChild(0).GetChild(0).GetChild(j).GetComponent<TMP_InputField>().
+                onValueChanged.RemoveListener(OnInputFieldValueChanged);
+        }
+        //销毁该ui
+        Destroy(uiPoint);
+
+        GameObject lastPoint = wayPoints.GetChild(count - 2).gameObject;
+        lastPoint.GetComponent<DroneWaypointScript>().waypointList[0] = wayPoints.GetChild(1).gameObject;
+        GameObject point = wayPoints.GetChild(count - 1).gameObject;
+        //检查下一个点是否是要销毁的对象
+        if (_drone.GetComponent<FlyingDroneScript>().nextWaypoint == point)
+        {
+            _drone.GetComponent<FlyingDroneScript>().nextWaypoint = wayPoints.GetChild(1).gameObject;
+        }
+        Destroy(point);
 
     }
 
